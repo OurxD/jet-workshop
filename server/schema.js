59c0d2d136f0ae70906d071cf6a681129a8a82e8
OurxD <donado.orlando@gmail.com>
2019-06-21 16:26:42 -0400

    add user schema

--- server/schema.js
+++ server/schema.js
@@ -1,5 +1,33 @@
 const { makeExecutableSchema } = require('graphql-tools');
 
+const USERS = [
+  {
+    id: 1,
+    name: 'one 🙉',
+    posts: [1],
+  },
+  {
+    id: 2,
+    name: '🙉',
+    posts: [],
+  },
+  {
+    id: 3,
+    name: 'one 🙉',
+    posts: [3],
+  },
+];
+const POSTS = [
+  {
+    id: 1,
+    title: 'first post title',
+  },
+  {
+    id: 2,
+    title: 'second post title',
+  },
+];
+
 const schema = makeExecutableSchema({
   typeDefs: `
     type Post {
@@ -7,16 +35,25 @@ const schema = makeExecutableSchema({
       title: String
     }
 
+    type User {
+      id: Int!
+      name: String
+      posts: [Int]
+    }
+
     type Query {
-      posts: [Post]
+      posts(id: Int): [Post]
+      users(id: Int): [User]
     }
   `,
   resolvers: {
     Query: {
-      posts: () => [{
-        id: 1,
-        title: 'mi titulo',
-      }],
+      posts: (_, { id }) => (id
+        ? [POSTS.find(({ id: postId }) => id === postId)]
+        : POSTS),
+      users: (_, { id }) => (id
+        ? [USERS.find(({ id: userId }) => id === userId)]
+        : USERS),
     },
   },
 });
